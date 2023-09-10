c70fe7ff908714198ac97cd92fa44ee59d95da9d
Arrrrrchi <esh6c4nm@gmail.com>
2023-09-10 13:56:37 +0900

    デシル分析

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -5,6 +5,7 @@ import { reactive, onMounted } from 'vue';
 import { getToday } from '@/common';
 import axios from 'axios';
 import Chart from '@/Components/Chart.vue';
+import ResultTable from '@/Components/ResultTable.vue';
 
 onMounted(() => {
     form.startDate = getToday();
@@ -31,6 +32,7 @@ const getData = async () => {
             data.data = res.data.data;
             data.labels = res.data.labels;
             data.totals = res.data.totals;
+            data.type = res.data.type;
             console.log(res.data);
         });
     } catch (e) {
@@ -55,30 +57,25 @@ const getData = async () => {
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         <form @submit.prevent="getData">
-                            From: <input type="date" name="startDate" v-model="form.startDate">
-                            To: <input type="date" name="endDate" v-model="form.endDate"><br>
-                            <button class="flex mt-4 mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
+                            <h3>分析方法</h3>
+                            <br>
+                            <div class="flex items-center">
+                                <input type="radio" class="mr-1" v-model="form.type" value="perDay" checked><span class="mr-4">日別</span>
+                                <input type="radio" class="mr-1" v-model="form.type" value="perMonth"><span class="mr-4">月別</span>
+                                <input type="radio" class="mr-1" v-model="form.type" value="perYear"><span class="mr-4">年別</span>
+                                <input type="radio" class="mr-1" v-model="form.type" value="decile"><span class="mr-4">デシル分析</span>
+                            </div>
+                            <br>
+                            <div class="flex items-center">
+                                From: <input type="date" name="startDate" v-model="form.startDate" class="mr-4">
+                                To: <input type="date" name="endDate" v-model="form.endDate" class="mr-10">
+                                <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
+                            </div>
                         </form>
-                        <div v-show="data.data">
+                        <div v-show="data.data" class="mt-6">
                             <Chart :data="data" />
                         </div>
-
-                        <div v-show="data.data" class="lg:w-2/3 w-full mx-auto overflow-auto mt-4">
-                                <table class="table-auto w-full text-left whitespace-no-wrap">
-                                    <thead>
-                                        <tr>
-                                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">年月日</th>
-                                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">金額</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr v-for="item in data.data" :key="item.date">
-                                            <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.date }}</td>
-                                            <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.total }}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                        <ResultTable :data="data" />
                     </div>
                 </div>
             </div>
